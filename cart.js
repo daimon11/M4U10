3d473539314fee3f500a8565766bdcab8f75e9f6
Dmitriy <104936927+daimon11@users.noreply.github.com>
2022-07-19 11:00:38 +0300

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,39 @@
+'use strict';
+const cart = {
+  items: [],
+  count: 0,
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+  add(productName, priceProduct, quantityProduct = 1) {
+    const product = {
+      productName,
+      priceProduct,
+      quantityProduct,
+    };
+    this.items.push(product);
+    this.increaseCount();
+    this.count += quantityProduct;
+  },
+  increaseCount() {
+    return this.count;
+  },
+  calculateItemPrice() {
+    return this.items.reduce((acc, {priceProduct, quantityProduct}) =>
+      acc + priceProduct * quantityProduct, 0);
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(JSON.stringify(this.totalPrice));
+  },
+};
+
+cart.add('Вино', 450);
+cart.add('Пиво', 100, 5);
+cart.totalPrice = 100;
+cart.print();
